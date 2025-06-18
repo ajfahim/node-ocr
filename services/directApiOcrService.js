2068b90ba3bbb1a8732bb8b8f8645fe037fa2708
AJ Fahim <32495838+ajfahim@users.noreply.github.com>
2025-06-18 21:41:03 +0600

    Use URL encoded body for token request (#6)

--- services/directApiOcrService.js
+++ services/directApiOcrService.js
@@ -174,12 +174,14 @@ async function getAccessToken(credentials) {
     const jwt = await generateJWT(credentials);
     
     // Exchange JWT for access token
+    const params = new URLSearchParams({
+      grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+      assertion: jwt,
+    });
+
     const response = await axios.post(
       'https://oauth2.googleapis.com/token',
-      {
-        grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
-        assertion: jwt
-      },
+      params.toString(),
       {
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded'
